50a5478df35e4ba1eb7444193b3e580b6c8bc57c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-26 04:43:28 +0530

    Add Provider and pass store as a prop

--- src/container/App.js
+++ src/container/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Row from '../components/Row';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <h1>
         Netflix Clone App
+        <Row>Netflix</Row>
       </h1>
-    </div>
+    </Provider>
   );
 }
 
