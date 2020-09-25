701a044d06cb9b37299e32f2b3c483bb8eca4676
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-26 04:47:36 +0530

    Add axios folder with fetching data logic

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,22 +1,26 @@
+import axios from 'axios'
+
 import {
   fetchMoviesCollectionSuccess,
   fetchMoviesCollectionRequest,
   fetchMoviesCollectionFailure
 } from '../redux/actions/moviesAction'
 
-export const fetchMovieCollection = () => {
 
+const fetchMovieCollection = () => {
   return (dispatch) => {
     dispatch(fetchMoviesCollectionRequest)
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then(response => {
         const movieData = response.data
+        console.log(response.data)
         dispatch(fetchMoviesCollectionSuccess(movieData))
       })
       .catch(error => {
         const errorMessage = error.message
         dispatch(fetchMoviesCollectionFailure(errorMessage))
       })
-
   }
 }
+
+export default fetchMovieCollection
