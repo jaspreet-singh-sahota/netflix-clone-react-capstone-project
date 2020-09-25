233e7e65ce233109a1483edeeab66e13db7a2965
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-26 01:13:12 +0530

    Add moviesReducer

---
+++ src/redux/recuders/moviesReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  collections: []
+};
+
+const moviesReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default moviesReducer;
