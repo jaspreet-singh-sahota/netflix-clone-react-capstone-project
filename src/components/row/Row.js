4ca7f2fd0710f484c8d745b72d2a538f4fde8fb4
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-26 23:21:07 +0530

    Fix row rendering logic

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
 import { useSelector } from "react-redux";
 import { image_url } from '../../axios/API_END_POINTS'
+import styles from './styles/Row.module.css'
 
 
-function Row({category, children, isNetflixRow }) {
+function Row({ category, children, isNetflixRow }) {
   const StoreInfo = useSelector(state => state.movies)
   const [movies] = useState(StoreInfo.movieCollection)
-  
+
   if (!movies[category]) {
     return null;
   }
 
   // console.log(movies)
-  
-  return (
-    <div className="row">
-      <h1 style={{color: 'white'}}>{children}</h1>
 
-      {movies[category].map((movie) => (
-            <img
-        key={movie.id}
-          src={`${image_url}${isNetflixRow ? movie.poster_path : movie.backdrop_path}`}
-        alt={movie.name}
-      />
-          ))
-      }
+  return (
+    <div className={styles.row}>
+      <h1 className={styles.title}>{children}</h1>
+
+      <div className={styles["row-container"]}>
+        {movies[category].map((movie) => (
+          <img
+            key={movie.id}
+            className={isNetflixRow ? styles.imageLarge : styles.image}
+            src={`${image_url}${isNetflixRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))
+        }
+      </div>
     </div>
   )
 }
