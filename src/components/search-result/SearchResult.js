5d125db7513e4208681ca1cfc368ce91ca69e873
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-28 00:25:38 +0530

    Add search result component

---
+++ src/components/search-result/SearchResult.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {image_url} from '../../axios/API_END_POINTS'
+import styles from './styles/searchResult.module.css'
+
+const SearchResult = ({movies}) => {
+  return (
+    <div>
+      {movies !== [] ? movies.map((movie) => (
+        <img
+          key={movie.id}
+          className={styles.image}
+          src={`${image_url}${movie.backdrop_path}`}
+          alt={movie.name}
+        />
+      )) : null
+      }
+    </div>
+  )
+}
+
+export default SearchResult
