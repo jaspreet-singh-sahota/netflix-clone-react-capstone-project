6ff00f8f3eb41b437dc76949ee45b952de6f376f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-29 01:32:00 +0530

    Refactor billboard row code

--- src/components/billboard-row/BillboardRow.js
+++ src/components/billboard-row/BillboardRow.js
@@ -6,20 +6,14 @@ import { GrPlayFill } from 'react-icons/gr'
 import { FiInfo } from 'react-icons/fi'
 
 const BillboardRow = () => {
-  const StoreInfo = useSelector(state => state.movieCategory)
-  const [movies] = useState(StoreInfo.movieCollection)
+  const moviesData = useSelector(state => state.movieCategory.movieCollection)
+  let FilteredMovies = moviesData.filter(movies => movies['NetflixOriginals'])?.[0]
 
-  const randomMovie = () => {
+  const randomKey = () => {
     return Math.floor(Math.random() * 20)
   }
 
-  const netflixOriginalsCollection = () => {
-    if (movies.NetflixOriginals) {
-      return movies.NetflixOriginals[randomMovie()]
-    }
-  }
-
-  const selectedMovie = netflixOriginalsCollection()
+  let selectedMovie = FilteredMovies ? (FilteredMovies['NetflixOriginals'][randomKey()]) : null
 
   function truncate(str, num) {
     return str?.length > num ? str.substr(0, num - 1) + "..." : str;
