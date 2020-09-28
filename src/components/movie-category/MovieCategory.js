03ed86b89edaeb71e7d5d7af9219e4eaea489dbf
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-28 10:15:21 +0530

    Add movie category page

---
+++ src/components/movie-category/MovieCategory.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Row from '../row/Row'
+
+const MovieCategory = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {/* <Row category={category}>{children}</Row> */}
+    </div>
+  )
+}
+
+export default MovieCategory
