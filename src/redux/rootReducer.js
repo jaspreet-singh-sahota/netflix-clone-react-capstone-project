24f3895fc39c9cb26fe0b0b1f4716e16640c401f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-29 02:03:42 +0530

    Update rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import  moviesCategoryReducer  from './reducers/movies-category-reducer/moviesCategoryReducer'
 import allMoviesReducer from "./reducers/all-movies-reducer/allMoviesReducer";
+import filter from "./reducers/filter";
 
 const rootReducer = combineReducers({
   movieCategory: moviesCategoryReducer,
-  allMovies: allMoviesReducer
+  allMovies: allMoviesReducer,
+  filter
 })
 
 export default rootReducer
