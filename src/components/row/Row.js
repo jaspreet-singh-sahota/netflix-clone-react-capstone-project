824a9f8ae07e6424c899c34f65a2e662e5a3fd43
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-29 01:58:29 +0530

    Add filtered category functionality to row component

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from "react-redux";
 import { image_url } from '../../axios/API_END_POINTS'
 import styles from './styles/Row.module.css'
@@ -6,20 +6,23 @@ import { Link } from 'react-router-dom';
 
 
 function Row({ category, children, isNetflixRow }) {
-  const StoreInfo = useSelector(state => state.movieCategory)
-  const [movies] = useState(StoreInfo.movieCollection)
+  const data = useSelector(state => state.movieCategory.movieCollection )
 
-  if (!movies[category]) {
-    return null;
+  let  FilteredMovies = data.filter(mov => mov[category])?.[0]
+  let movieKeys
+  if (FilteredMovies) {
+    movieKeys = Object.keys(FilteredMovies[category])
   }
-
+  
   return (
     <div className={styles.row}>
       <h1 className={styles.title}>{children}</h1>
 
       <div className={styles["row-container"]}>
-        {movies[category].map((movie) => (
-          <Link key={movie.id} className={styles.link} to={{pathname: `movie/${movie.id}`, state: {movie}}}>
+        {movieKeys?.map((key) => {
+          const movie = FilteredMovies[category][key]
+          return(
+          <Link key={movie.id} className={styles.link} to={{ pathname: `movie/${movie.id}`, state: { movie } }}>
             <img
               key={movie.id}
               className={isNetflixRow ? styles.imageLarge : styles.image}
@@ -27,7 +30,7 @@ function Row({ category, children, isNetflixRow }) {
               alt={movie.name}
             />
           </Link>
-        ))
+        )})
         }
       </div>
     </div>
