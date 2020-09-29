520ae2c26c89e483fa667a01daf379ffc06676b7
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-29 16:52:24 +0530

    Add video functionality

--- src/pages/movie-show-page/Movie.js
+++ src/pages/movie-show-page/Movie.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoMdPlay, IoMdAdd } from 'react-icons/io';
 import PropTypes from 'prop-types';
 import { IMAGE_URL } from '../../axios/API_END_POINTS';
 import styles from './styles/Movie.module.css';
+import movieTrailer from 'movie-trailer'
+import YouTube from 'react-youtube'
 
 const Movie = ({ location }) => {
   const { movie } = location.state;
+  const [trailerUrl, setTrailerUrl] = useState('')
+  const [isPlaying, setIsPlaying] = useState(false)
 
   if (!movie) {
     return null;
@@ -16,36 +20,42 @@ const Movie = ({ location }) => {
   }
 
   return (
-    <div className={styles.container}>
-      <div className={styles['info-container']}>
-        <h1 className={styles.text}>
-          {(movie.name ? truncate(movie.name, 25) : null)
-          || (movie.title ? truncate(movie.title, 25) : null)}
-        </h1>
-        <h2 className={styles['movie-info']}>{truncate(movie.overview, 385)}</h2>
-        <div className={styles['button-container']}>
-          <div className={styles.buttons}>
-            <button type="submit" className={styles['play-button']}>
-              <IoMdPlay className={styles.icons} />
-              {' '}
-              PLAY
+    <div>
+      <div className={styles.container}>
+        <div className={styles['info-container']}>
+          <h1 className={styles.text}>
+            {
+              (movie.name ? truncate(movie.name, 25) : null)
+              || (movie.title ? truncate(movie.title, 25) : null)
+            }
+          </h1>
+          <h2 className={styles['movie-info']}>{truncate(movie.overview, 385)}</h2>
+          <div className={styles['button-container']}>
+            <div className={styles.buttons}>
+              <button type="submit" onClick={() => handleMovieUrl(movie)} className={styles['play-button']}>
+                <IoMdPlay className={styles.icons} />
+                {' '}
+                PLAY
             </button>
-            <button type="submit" className={styles['list-button']}>
-              <IoMdAdd className={styles.icons} />
-              {' '}
-              MY LIST
+              <button type="submit" className={styles['list-button']}>
+                <IoMdAdd className={styles.icons} />
+                {' '}
+                MY LIST
             </button>
+            </div>
           </div>
         </div>
+        <div className={styles.block} />
+        <div className={styles['image-container']}>
+          <img
+            className={styles.image}
+            src={`${IMAGE_URL}${movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        </div>
       </div>
-      <div className={styles.block} />
-      <div className={styles['image-container']}>
-        <img
-          className={styles.image}
-          src={`${IMAGE_URL}${movie.backdrop_path}`}
-          alt={movie.name}
-        />
-      </div>
+      {isPlaying ? <div onClick={handleStopVideo} className={styles.modal}></div> : null}
+      {isPlaying  && <YouTube className={styles.video} videoId={trailerUrl} opts={opts} onReady={handlePlayVideo} />}
     </div>
   );
 };
