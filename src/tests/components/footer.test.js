b14bc5e51b06618b9f099b46d1fc19e44a21002f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-04 11:13:11 +0530

    Add footer test with snapshots

---
+++ src/tests/components/footer.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from '../../components/footer/Footer';
+import {
+  ImFacebook2, ImInstagram, ImTwitter, ImYoutube,
+} from 'react-icons/im';
+
+describe('Footer', () => {
+  it('should render a facebook icon', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find(ImInstagram)).toHaveLength(1);
+  });
+
+
+  it('should render a instagram icon', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find(ImFacebook2)).toHaveLength(1);
+  });
+
+  it('should render a Twitter icon', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find(ImTwitter)).toHaveLength(1);
+  });
+
+  it('should render a YouTube icon', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find(ImYoutube)).toHaveLength(1);
+  });
+
+  it('matches Footer snapshot', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper).toMatchSnapshot();
+  });
+})
